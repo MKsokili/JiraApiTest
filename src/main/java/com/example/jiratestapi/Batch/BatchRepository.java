d2eb11e2e2bcfea86b1a00a6b8b6a03531dcb8e5
chaimae <chaimaer759@gmail.com>
2024-08-01 11:19:08 +0200

    get batch Tickets Of Last Batch

--- src/main/java/com/example/jiratestapi/Batch/BatchRepository.java
+++ src/main/java/com/example/jiratestapi/Batch/BatchRepository.java
@@ -2,13 +2,17 @@ package com.example.jiratestapi.Batch;
 
 import com.example.jiratestapi.BatchTicket.BatchTicket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BatchRepository extends JpaRepository<Batch, Long> {
-
-
+    @Query("SELECT b FROM Batch b ORDER BY b.startedDate DESC")
+    List<Batch> findAllBatchesOrderedByStartedDateDesc();
 }
