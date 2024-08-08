d6e24d38e5408a8bdc5c9c795e16f0986f084a1c
MKsokili <mksokili20@gmail.com>
2024-08-08 16:14:30 +0100

    get batchTicket by Id

--- src/main/java/com/example/jiratestapi/BatchTicket/BatchTicketController.java
+++ src/main/java/com/example/jiratestapi/BatchTicket/BatchTicketController.java
@@ -3,31 +3,34 @@ package com.example.jiratestapi.BatchTicket;
 import com.example.jiratestapi.Batch.Batch;
 import com.example.jiratestapi.Projects.Project;
 import com.example.jiratestapi.Projects.ProjectRepository;
+import com.example.jiratestapi.Task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/batchTickets")
+@CrossOrigin(origins = "http://localhost:3000")
 public class BatchTicketController {
+
     @Autowired
     private ProjectRepository projectRepository;
-    @GetMapping("/batchTicketByProjectId/{projectId}")
-    public List<Batch> getBatchTicketsByProjectId(@PathVariable Long projectId) {
-        List<Batch> batchs;
-        try {
-            Optional<Project> project = projectRepository.findById(projectId);
-            batchs=project.get().getBatches();
-        } catch (Exception e) {
-            throw new RuntimeException("error while getting the batch tickets using the project id");
-        }
-        return batchs;
-    }
+    @Autowired
+    private BatchTicketRepository batchTicketRepository;
 
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BatchTicket> getBatchTicketById(@PathVariable Long id) {
+        BatchTicket batchTicket = batchTicketRepository.findById(id).get();
+        if (batchTicket != null) {
+            return ResponseEntity.ok(batchTicket);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
