879351f32ffbb7b9289ca6212c1a3a461a57281d
chaimae <chaimaer759@gmail.com>
2024-08-13 07:39:52 +0200

    get tasks by project ID

--- src/main/java/com/example/jiratestapi/Projects/ProjectController.java
+++ src/main/java/com/example/jiratestapi/Projects/ProjectController.java
@@ -1,5 +1,6 @@
 package com.example.jiratestapi.Projects;
 
+import com.example.jiratestapi.Task.Task;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +34,19 @@ public class ProjectController {
         List<Project> projects=projectRepository.findAll();
         return new ResponseEntity<>(projects, HttpStatus.OK);
 
+    }
+    @GetMapping("/{projectId}/tasks")
+    public List<Task> getTasksByProjectId(@PathVariable Long projectId){
+        Optional<Project> project= projectRepository.findById(projectId);
+        List<Task> tasks;
+        if (project.isPresent()){
+            Project p=project.get();
+            tasks=project.get().issues;
+        }
+        else{
+            throw new RuntimeException("Projeect with this ID  don't exist");
+        }
+        return  tasks;
     }
 
 
